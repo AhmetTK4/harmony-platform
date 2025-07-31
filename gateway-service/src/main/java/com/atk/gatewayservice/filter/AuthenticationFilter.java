31e12f1eabe0d35bd14714857edeae34e2d9b12f
Ahmet <AhmetTK4@users.noreply.github.com>
2025-07-31 21:18:16 +0300

    adding jwt gateway service

--- gateway-service/src/main/java/com/atk/gatewayservice/filter/AuthenticationFilter.java
+++ gateway-service/src/main/java/com/atk/gatewayservice/filter/AuthenticationFilter.java
@@ -2,10 +2,12 @@ package com.atk.gatewayservice.filter;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
@@ -13,12 +15,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> implements Ordered {
 
     @Value("${jwt.secret}")
-    private String secretKey;
+    private String secretKeyString;
 
     public static class Config {}
 
@@ -27,16 +33,21 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-
+            log.info("Incoming request URI: {}", exchange.getRequest().getURI());
+            log.info("Auth header: {}", authHeader);
             if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                log.warn("Missing or invalid Authorization header");
                 return onError(exchange, "Missing or invalid Authorization header", HttpStatus.UNAUTHORIZED);
             }
 
             String token = authHeader.substring(7);
 
             try {
-                Claims claims = Jwts.parser()
-                        .setSigningKey(secretKey)
+
+                Key key = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(key)
+                        .build()
                         .parseClaimsJws(token)
                         .getBody();
 
@@ -45,6 +56,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                         .build();
 
             } catch (Exception e) {
+                log.error("Invalid JWT Token: {}", e.getMessage());
                 return onError(exchange, "Invalid JWT Token: " + e.getMessage(), HttpStatus.UNAUTHORIZED);
             }
 
@@ -53,12 +65,13 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        log.warn("Error in filter: {} - {}", httpStatus, err);
         exchange.getResponse().setStatusCode(httpStatus);
         return exchange.getResponse().setComplete();
     }
 
     @Override
     public int getOrder() {
-        return -1; // high priority
+        return -1;
     }
 }
